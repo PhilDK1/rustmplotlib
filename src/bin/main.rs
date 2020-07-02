6de03e8363868da60972ca581d8fc82b7592fecf
PhilDK1 <philipdkrause@gmail.com>
2020-07-02 23:12:17 +0100

    file structure changed

--- src/bin/main.rs
+++
@@ -1,9 +0,0 @@
-extern crate mplot;
-
-fn main() {
-    let ans: i32 = mplot::builtins_sum(vec![1,2, 3, 5, 9]).unwrap();
-    println!("builtins ans: {}", ans);
-    let np_ans: i32 = mplot::numpy_sum(vec![1,2, 3, 5, 9]).unwrap();
-    println!("np ans: {}", np_ans);
-}
-
