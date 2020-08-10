c73fa64ee0c8333f9b2b1f8a17338bf67d76e8e2
PhilDK1 <philipdkrause@gmail.com>
2020-08-10 15:32:57 +0100

    removed test function

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,6 @@ mod plot;
 
 pub use plot::*;
 
-// two test functions
-pub fn builtins_sum(vec:Vec<i32>) -> PyResult<i32> {
-    let gil = Python::acquire_gil();
-    let py = gil.python();
-    let builtins = PyModule::import(py, "builtins")?;
-    let total: i32 = builtins.call1("sum", (vec,))?.extract()?;
-    Ok(total)
-}
-
-
-pub fn numpy_sum(vec: Vec<i32>) -> PyResult<i32> {
-    let gil = Python::acquire_gil();
-    let py = gil.python();
-    let np = PyModule::import(py, "numpy")?;
-    let total: i32 = np.call1("sum", (vec,))?.extract()?;
-    Ok(total)
-}
 /*
 enum Marker {
     // acceptable arguments for fmt marker refer to api in PyPlot struct
