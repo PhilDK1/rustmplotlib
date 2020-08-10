b9121b46b26ba6a387f968de75bd97d08cee5546
PhilDK1 <philipdkrause@gmail.com>
2020-08-10 15:31:55 +0100

    removed commented old code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 use mplot;
 
 fn main() {
-    // let ans: i32 = mplot::builtins_sum(vec![1,2, 3, 5, 9]).unwrap();
-    // println!("builtins ans: {}", ans);
-    // let np_ans: i32 = mplot::numpy_sum(vec![1,2, 3, 5, 9]).unwrap();
-    // println!("np ans: {}", np_ans);
-    // let vec: Vec<u32> = Vec::new();
-    // let pyplt = mplot::PyPlot::new(&vec[..],"y".to_string());
-    // let ans = pyplt.plot();
     let x = vec![1.,2., 3.];
     let y = vec![1., 4., 9.];
     let y_2 = vec![1., 2., 3.];
