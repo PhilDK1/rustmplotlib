59eae015548137d5a67750a0a0dff81c41992328
PhilDK1 <philipdkrause@gmail.com>
2020-08-11 19:25:46 +0100

    x and y label setter and getter methods on axes

--- src/figax/axes.rs
+++ src/figax/axes.rs
@@ -10,6 +10,8 @@ use crate::common::Env;
 pub struct Axes<'a, T> {
     plot_data: Option<PlotData<'a, T>>,
     title: Option<String>, // https://matplotlib.org/3.2.2/api/_as_gen/matplotlib.axes.Axes.set_title.html#matplotlib-axes-axes-set-title
+    xlabel: Option<String>,
+    ylabel: Option<String>,
 
 }
 
@@ -18,6 +20,8 @@ impl<'a, T> Axes<'a, T> {
         Axes::<T> {
             plot_data: None,
             title: None,
+            xlabel: None,
+            ylabel: None,
         }
     }
 
@@ -27,15 +31,39 @@ impl<'a, T> Axes<'a, T> {
     }
 
     pub fn get_title(&self) -> Result<String, &'static str> {
-        // https://matplotlib.org/3.2.2/api/_as_gen/matplotlib.axes.Axes.set_title.html#matplotlib-axes-axes-set-title
+        // https://matplotlib.org/3.2.2/api/_as_gen/matplotlib.axes.Axes.get_title.html#matplotlib-axes-axes-get-title
         match &self.title {
             Some(set_title) => Ok(set_title.to_string()),
             None => Err("No title set, try: Axis.set_title(title: &str)"),
-        }
-        
+        } 
+    }
+
+    pub fn set_xlabel(&mut self, xlabel: &str) {
+        // https://matplotlib.org/3.2.2/api/_as_gen/matplotlib.axes.Axes.set_xlabel.html#matplotlib.axes.Axes.set_xlabel
+        self.xlabel = Some(xlabel.to_owned());
+    }
+
+    pub fn get_xlabel(&self) -> Result<String, &'static str> {
+        // https://matplotlib.org/3.2.2/api/_as_gen/matplotlib.axes.Axes.get_xlabel.html#matplotlib.axes.Axes.get_xlabel
+        match &self.xlabel {
+            Some(set_xlabel) => Ok(set_xlabel.to_string()),
+            None => Err("No xlabel set, try: Axis.set_xlabel(xlabel: &str)"),
+        } 
     }
 
 
+    pub fn set_ylabel(&mut self, ylabel: &str) {
+        // https://matplotlib.org/3.2.2/api/_as_gen/matplotlib.axes.Axes.set_ylabel.html#matplotlib.axes.Axes.set_ylabel
+        self.ylabel = Some(ylabel.to_owned());
+    }
+
+    pub fn get_ylabel(&self) -> Result<String, &'static str> {
+        // https://matplotlib.org/3.2.2/api/_as_gen/matplotlib.axes.Axes.get_xlabel.html#matplotlib.axes.Axes.get_xlabel
+        match &self.ylabel {
+            Some(set_ylabel) => Ok(set_ylabel.to_string()),
+            None => Err("No ylabel set, try: Axis.set_ylabel(ylabel: &str)"),
+        } 
+    }
 }
 
 // #[derive(Debug)]
