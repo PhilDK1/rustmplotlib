917bbc4c41730557cdabe9bccc718ba48c97464c
PhilDK1 <philipdkrause@gmail.com>
2020-08-13 14:23:50 +0100

    comments added

--- src/common.rs
+++ src/common.rs
@@ -1,11 +1,17 @@
 use pyo3::prelude::*;
 
+/// prehaps have a way to specify a particular environment and check if mpl
+/// is installed as well as the particular version. Need to check the pyo3
+/// docs to determine if that is possible
+
+// represents Python environment
 pub struct Env {
     pub gil: GILGuard,
 }
 
 impl Env {
     pub fn new() -> Env {
+        // gets the environement
         Env {
             gil: Python::acquire_gil(),
         }
@@ -13,6 +19,7 @@ impl Env {
 }
 
 impl Default for Env {
+    // clippy told me to
     fn default() -> Self {
         Self::new()
     }
