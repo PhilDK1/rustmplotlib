c0ba163606c703de03b7c6896edbce9b3719baf7
PhilDK1 <philipdkrause@gmail.com>
2020-08-23 22:20:19 +0100

    added markerstyle

--- src/plots/scatter.rs
+++ src/plots/scatter.rs
@@ -1,13 +1,13 @@
 use pyo3::prelude::*;
 use pyo3::types::*;
 use crate::addition_objs::colormap::Colormap;
-
+use crate::addition_objs::markerstyle::MarkerStyle;
 pub struct Scatter<'p, T: pyo3::conversion::ToPyObject> {
     // https://matplotlib.org/3.2.2/api/_as_gen/matplotlib.axes.Axes.scatter.html#matplotlib.axes.Axes.scatter
     x_data: &'p [T],
     y_data: &'p [T],
     // cannot use as they take a obj in python don't know how to convert yet:
-    // marker_style: Option<String>,
+    marker_style: Option<MarkerStyle>,
     cmap: Option<Colormap>,
 }
 
@@ -17,7 +17,7 @@ impl<'p, T: pyo3::conversion::ToPyObject> Scatter<'p, T> {
         Scatter {
             x_data: &x,
             y_data: &y,
-            // marker_style: None,
+            marker_style: None,
             cmap: None,
         }
     }
@@ -39,17 +39,18 @@ impl<'p, T: pyo3::conversion::ToPyObject> Scatter<'p, T> {
             vec![self.x_data.to_owned(), self.y_data.to_owned()].into_iter(),
         )
     }
-/*
-    pub fn set_marker(&mut self, markerstyle: String) {
+
+    pub fn set_marker(&mut self, markerstyle: MarkerStyle) {
         self.marker_style = Some(markerstyle);
     }
 
     pub fn set_cmap(&mut self, cmap: Colormap) {
         self.cmap = Some(cmap);
     }
-*/
+
     pub fn get_plot_kwargs(&self, py: Python<'p>, mpl: &'p PyModule) -> &PyDict {
         let new_dict = PyDict::new(py);
+
         match &self.cmap {
             Some(colormap) => {
                 let options = PyDict::new(py);
@@ -63,13 +64,27 @@ impl<'p, T: pyo3::conversion::ToPyObject> Scatter<'p, T> {
                 new_dict.set_item("cmap", cmap)
             },
             None =>new_dict.set_item("cmap", py.None()),
-        }.expect("Err of some kind in colormap of src/figax/plots.rs:: Scatter.get_plot_kwargs()");
+        }.expect("Err of some kind in colormap vals of rc/plots/scatter.rs Scatter.get_plot_kwargs()");
 
+        match &self.marker_style {
+            Some(markerstyle) => {
+                let options = PyDict::new(py);
+                
+                match &markerstyle.marker {
+                    Some(str_marker) => options.set_item("marker", str_marker.to_string()),
+                    None => options.set_item("marker", py.None()),
+                }.expect("Err of some kind in markerstyle vals of src/plots/scatter.rs Scatter.get_plot_kwargs()");
 
-        // match &self.marker_style {
-        //     Some(marker) => new_dict.set_item("marker", marker),
-        //     None => new_dict.set_item("marker", py.None()),
-        // };
+                match &markerstyle.fillstyle {
+                    Some(fillstyle_str) => options.set_item("fillstyle", fillstyle_str.to_string()),
+                    None => options.set_item("fillstyle", py.None()),
+                }.expect("Err of some kind in markerstyle vals of src/plots/scatter.rs Scatter.get_plot_kwargs()");
+                let markerstyling = mpl.call_method("markers.MarkerStyle", (), Some(options)).unwrap();
+                new_dict.set_item("marker", markerstyling)
+            },
+            None => new_dict.set_item("marker", py.None()),
+        }.expect("Err of some kind in markerstyle vals of src/plots/scatter.rs Scatter.get_plot_kwargs()");
+
         new_dict
     }
 }
